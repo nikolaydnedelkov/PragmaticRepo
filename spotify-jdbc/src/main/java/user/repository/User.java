db7dd8d6febc926b62841a9d9ef9ff8bbb510006
Nikolay Nedelkov <nikolayd.nedelkov@gmail.com>
2020-02-20 22:13:19 +0200

    lombok fix

--- spotify-jdbc/src/main/java/user/repository/User.java
+++ spotify-jdbc/src/main/java/user/repository/User.java
@@ -1,15 +1,18 @@
 package user.repository;
 
-import lombok.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class User {
+
     private long id;
     private String firstName;
     private String lastName;
     private String email;
     private LocalDate birthDate;
     private boolean hasPaidApp;
+
 }
+
